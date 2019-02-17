6485a9c65d328d2061996b2599eda9535c409076
shayneofficer <shayneofficer@gmail.com>
2019-02-16 19:26:11 -0600

    99 bottles of beer on the wall example

---
+++ BeersOnWall.java
@@ -0,0 +1,26 @@
+public class BeersOnWall {
+
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while(beerNum > 0) {
+            
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.\nPass it around.");
+            beerNum--;
+            
+            if(beerNum == 1) {
+                word = "bottle";
+            }
+            
+            if(beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall.");
+            } else {
+                System.out.println("No more bottles of beer on the wall.");
+            }
+
+        }
+    }
+}
