7956779391e70a2cfa986121c32d41c4cd5dc128
shayneofficer <shayneofficer@gmail.com>
2019-02-16 19:31:13 -0600

    added some comments, gitignore for .class files, etc

--- BeersOnWall.java
+++ BeersOnWall.java
@@ -4,18 +4,24 @@ public class BeersOnWall {
         int beerNum = 99;
         String word = "bottles";
 
-        while(beerNum > 0) {
-            
+        while (beerNum > 0) {
+
+            // if(beerNum == 1) {
+            // word = "bottle";
+            // }
+
             System.out.println(beerNum + " " + word + " of beer on the wall");
             System.out.println(beerNum + " " + word + " of beer.");
             System.out.println("Take one down.\nPass it around.");
             beerNum--;
-            
-            if(beerNum == 1) {
+
+            // Move the word change here after the adjustment, otherwise the last iteration
+            // is wrong.
+            if (beerNum == 1) {
                 word = "bottle";
             }
-            
-            if(beerNum > 0) {
+
+            if (beerNum > 0) {
                 System.out.println(beerNum + " " + word + " of beer on the wall.");
             } else {
                 System.out.println("No more bottles of beer on the wall.");
